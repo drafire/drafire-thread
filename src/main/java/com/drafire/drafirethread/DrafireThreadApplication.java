bf02d952cfec175cd8a722430b87baf03bfe0715
drafire <hyljljlj@163.com>
2019-04-05 19:35:14 +0800

    Initial commit

---
+++ src/main/java/com/drafire/drafirethread/DrafireThreadApplication.java
@@ -0,0 +1,13 @@
+package com.drafire.drafirethread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrafireThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrafireThreadApplication.class, args);
+	}
+
+}
